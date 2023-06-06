4f527be518b03ece157e903bcc13cd2604ad38b2
444sofM <ana.mezu@correounivalle.edu.co>
2023-06-05 22:54:20 -0500

    DATOS PERSONALES

--- src/controladorVentanas/ControladorPrestamos/ControladorModificarPrestamo.java
+++ src/controladorVentanas/ControladorPrestamos/ControladorModificarPrestamo.java
@@ -1,4 +1,7 @@
-
+/*NOMPRE DE PARTICIPANTES :Miguel Gonzales , Ana Mezu ,Vanesa Chavez . 
+PROFESOR: Luis Romo.
+GRUPO DE FPOE:01.
+MINI PROYECTO 3.*/
 package controladorVentanas.ControladorPrestamos;
 
 import Vista.Prestamo.ModificarPrestamo;
--- src/controladorVentanas/ControladorRecursos/ControladorAgregarRecurso.java
+++ src/controladorVentanas/ControladorRecursos/ControladorAgregarRecurso.java
@@ -1,4 +1,7 @@
-
+/*NOMPRE DE PARTICIPANTES :Miguel Gonzales , Ana Mezu ,Vanesa Chavez . 
+PROFESOR: Luis Romo.
+GRUPO DE FPOE:01.
+MINI PROYECTO 3.*/
 package controladorVentanas.ControladorRecursos;
 
 import Vista.Recursos.AgregarRecurso;
